f7f868f446127ea517ea1c6eb907ff0763b323e8
Yash Vardhan Rawat <undergod.code@gmail.com>
2024-09-26 00:25:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-// import logo from './logo.svg';
+
 import './App.css';
 import TicTacToe from './components/tic-tac-toe';
-// import connectthedots from './components/connect-the-dots';
-import ConnectTheDots from './components/connect-the-dots';
-// import PlayerForm from './components/connect-the-dots/playerform';
-import ConnectTheDotsdebug from './components/connect-the-dots/debugger';
-
 function App() {
   return (
     <div className="App">
